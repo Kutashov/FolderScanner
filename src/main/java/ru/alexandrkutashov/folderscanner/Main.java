b0a00d9ac828340fb83eaa99aa397e10950f04eb
Alexandr Kutashov <kutashov.alexandr@yandex.ru>
2018-07-09 20:38:51 +0300

    Add params processing

--- src/main/java/ru/alexandrkutashov/folderscanner/Main.java
+++ src/main/java/ru/alexandrkutashov/folderscanner/Main.java
@@ -21,6 +21,18 @@ public class Main {
 
     private static final Logger logger = LoggerFactory.getLogger(Main.class);
 
+    //region params
+    private static final String MONITOR_FOLDER_PARAM = "-m";
+    private static final String HANDLED_FILES_FOLDER_PARAM = "-h";
+    private static final String MONITOR_PERIOD_SEC_PARAM = "-p";
+    private static final int PERIOD_DEFAULT = 0;
+    private static final String FOLDER_DEFAULT = null;
+
+    private static String monitorFolder = FOLDER_DEFAULT;
+    private static int monitorPeriod = PERIOD_DEFAULT;
+    private static String handledFilesFolder = FOLDER_DEFAULT;
+    //endregion
+
     //region dependencies
     private static final ScheduledExecutorService scheduler = Executors.newScheduledThreadPool(1);
     private static final Queue<File> filesQueue = new ConcurrentLinkedQueue<>();
@@ -34,20 +46,52 @@ public class Main {
     public static void main(String[] args) {
 
         try {
+            processParams(args);
+
             //launch cores-1 worker thread and one for handling db
             for (int i = 0; i < cores - 1; ++i) {
                 schedulerThreads.execute(new WorkTask(filesQueue, foldersQueue, databaseQueue,
                         symbolicLinkSet, new XmlFileConverter()));
             }
             schedulerThreads.execute(new HandlerTask(databaseQueue, new DB(),
-                    new FileMover("C:\\Users\\Alexandr\\Desktop")));
+                    new FileMover(handledFilesFolder)));
 
             //schedule checking
-            scheduler.scheduleAtFixedRate(new StartTask(filesQueue, foldersQueue, "C:\\Programming\\android-sdk"),
-                    0, 8, TimeUnit.SECONDS);
-        } catch (SQLException | JAXBException | ClassNotFoundException e) {
+            scheduler.scheduleAtFixedRate(new StartTask(filesQueue, foldersQueue, monitorFolder),
+                    0, monitorPeriod, TimeUnit.SECONDS);
+        } catch (SQLException | JAXBException | ClassNotFoundException | IllegalArgumentException e) {
             logger.error(e.getMessage());
             System.exit(-1);
+        }
+    }
+
+    private static void processParams(String[] args) {
+        for (int i = 0; i < args.length; ++i) {
+            final String arg = args[i];
+
+            switch (arg) {
+                case MONITOR_FOLDER_PARAM:
+                    monitorFolder = args[++i];
+                    break;
+                case HANDLED_FILES_FOLDER_PARAM:
+                    handledFilesFolder = args[++i];
+                    break;
+                case MONITOR_PERIOD_SEC_PARAM:
+                    monitorPeriod = Integer.valueOf(args[++i]);
+                    break;
+                default:
+                    logger.error("Unknown param: " + arg);
+            }
+        }
+
+        if (monitorFolder == FOLDER_DEFAULT) {
+            throw new IllegalArgumentException("Monitor folder param (" + MONITOR_FOLDER_PARAM + ") is not set");
+        }
+        if (handledFilesFolder == FOLDER_DEFAULT) {
+            throw new IllegalArgumentException("Handled files folder param (" + HANDLED_FILES_FOLDER_PARAM + ") is not set");
+        }
+        if (monitorPeriod == PERIOD_DEFAULT) {
+            throw new IllegalArgumentException("Monitor folder param (" + MONITOR_PERIOD_SEC_PARAM + ") is not set");
         }
     }
 }
